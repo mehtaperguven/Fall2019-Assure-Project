83d5c721703ea5020920043832ecbb384182464c
mehtaperguven <mehtaperguven13@gmail.com>
2020-07-07 00:44:59 -0500

    test added

--- src/test/java/com/automation/tests/homeworksAPI/HomeWork2.java
+++ src/test/java/com/automation/tests/homeworksAPI/HomeWork2.java
@@ -1,6 +1,7 @@
 package com.automation.tests.homeworksAPI;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.baseURI;
 
@@ -9,5 +10,9 @@ public class HomeWork2 {
     @BeforeAll
     public static void setup() {
         baseURI = "https://cybertek-ui-names.herokuapp.com/api/";
+    }
+    @Test
+    public static void test1(){
+
     }
 }
